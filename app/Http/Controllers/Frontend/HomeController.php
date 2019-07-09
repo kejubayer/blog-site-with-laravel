4e46fb727406d18dbcbf9b728c9b58fe948dc328
Eftakhar Jubayer <khondokereftakharjubayer@gmail.com>
2019-07-09 12:27:50 +0600

    frontend layout developed

---
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function showHome()
+    {
+        $data = [];
+        $data['current_time'] = date('Y-m-d , H:i:s');
+        $data['links'] = [
+            'Facebook' => 'https://facebook.com',
+            'Twitter' => 'https://twitter.com',
+            'Google' => 'https://google.com',
+            'Youtube' => 'https://youtube.com',
+            'LinkedIn' => 'https://linkedin.com'
+        ];
+        return view('frontend.home',$data);
+    }
+
+    public function post()
+    {
+        $data = [];
+        $data['current_time'] = date('Y-m-d , H:i:s');
+        $data['links'] = [
+            'Facebook' => 'https://facebook.com',
+            'Twitter' => 'https://twitter.com',
+            'Google' => 'https://google.com',
+            'Youtube' => 'https://youtube.com',
+            'LinkedIn' => 'https://linkedin.com'
+        ];
+        return view('frontend.post',$data);
+    }
+
+
+}
